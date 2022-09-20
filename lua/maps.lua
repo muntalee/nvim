fa130f61a7afd84a06c4a56827faa8b17beb6452
krakenegg101 <islammuntasirul@gmail.com>
2022-09-19 22:27:24 -0400

    qol updates

---
+++ lua/maps.lua
@@ -0,0 +1,38 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true }
+
+-- Disable arrow keys
+keymap('', '<Up>', '<Nop>', {})
+keymap('', '<Down>', '<Nop>', {})
+keymap('', '<Left>', '<Nop>', {})
+keymap('', '<Right>', '<Nop>', {})
+
+-- Pressing ii to escape
+keymap('i', 'ii', '<Esc>', {})
+keymap('v', 'ii', '<Esc>', {})
+
+keymap('', 'gf', '<C-W>gf', {})
+
+-- Split nav
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
+
+-- Split resizing
+keymap('n', '>', '<C-w>>', opts)
+keymap('n', '<', '<C-w><', opts)
+
+-- New Tab
+keymap('n', 'te', ':tabedit<Return>', { silent = true })
+
+-- Splitting
+keymap('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+
+-- Pasting
+keymap('n', 'y', '"+y', opts)
+keymap('v', 'y', '"+y', opts)
+
+-- Tree
+keymap('n', '<leader><Shift>t', ':NvimTreeToggle<cr>', opts)
